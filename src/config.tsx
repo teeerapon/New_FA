408705b716aabc535c301ce11f6404f08288b75f
Teerapon <the_trp@hotmail.com>
2025-01-23 14:02:50 +0700

    commit NEW FA 23_01_2025

--- src/config.tsx
+++ src/config.tsx
@@ -1,6 +1,6 @@
 const httpPTEC_TEST = 'http://similan:33052/api' //main
 const httpLandOffice = 'http://10.20.100.29:33052/api' //TEST
-const httpHome = 'http://172.20.10.2:33052/api' //Home
+const httpHome = 'http://172.16.3.143:33052/api' //Home
 const wifioffice = 'http://10.20.105.104:33052/api'
 
 interface DataConfig {
@@ -22,5 +22,5 @@ export const dataConfig: DataConfig = {
     'Content-Type': `multipart/form-data`,
     'Accept': 'application/json'
   },
-  http: httpLandOffice
+  http: httpPTEC_TEST
 }
